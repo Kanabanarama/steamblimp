07b6e33839f120e0f78b7f77063c776dded70a79
Kana <kanabanarama@googlemail.com>
2013-11-30 13:04:45 +0100

    scenes

--- src/game.js
+++ src/game.js
@@ -1,8 +1,11 @@
 Game = {
+	width: 1000,
+	height: 600,
 	init: function () {
-		Crafty.init(1000, 600, document.getElementById('canvas'));
+		Crafty.init(Game.width, Game.height, document.getElementById('canvas'));
 		Crafty.background('gray');
-		Game.start();
+		Game.initScenes();
+		Crafty.scene('Loading');
 	},
 	lockPointer: function(element) {
 		var havePointerLock = 'pointerLockElement' in document ||
@@ -13,10 +16,29 @@ Game = {
 			element.webkitRequestPointerLock;
 		return element.requestPointerLock();
 	},
-	start: function() {
-		var player = Crafty.e('Player').attr({'x': 50, 'y': 50, 'h': 60, 'w': 30}).fourway(5).color('#dddddd');
-		var turret = Crafty.e('2D, Canvas, Color').attr({'x': 80, 'y': 100, 'h': 10, 'w': 10}).color('#ff0000');
+	initScenes: function() {
+		Crafty.scene('Loading', function() {
+			var loadingText = 'Loading blimbety blimp stuff...';
+			Crafty.e('2D, DOM, Text')
+				.text(loadingText)
+				.attr({ x: Game.width/2 - loadingText.length*5/2, y: Game.height/2 - 24, w: Game.width });
 
-		player.attach(turret);
+			Crafty.load(['assets/blimp.png'], function() {
+				Crafty.sprite(60, 'assets/blimp.png', {
+					blimp_rank_01: [0, 0],
+					blimp_rank_02: [1, 0],
+					blimp_rank_03: [0, 1],
+					blimp_rank_04: [1, 1]
+				});
+
+				//Crafty.scene('Main');
+			})
+		});
+
+		Crafty.scene('Main', function() {
+			var player = Crafty.e('Player').attr( {'x': 50, 'y': 50, 'h': 60, 'w': 30} ).fourway(5).color('#dddddd');
+			var turret = Crafty.e('2D, Canvas, Color').attr( {'x': 80, 'y': 100, 'h': 10, 'w': 10} ).color('#ff0000');
+			player.attach(turret);
+		});
 	}
 };
