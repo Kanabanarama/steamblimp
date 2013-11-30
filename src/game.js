8cb273002a7d20b0408322a9e3427e36caf7e144
Kana <kanabanarama@googlemail.com>
2013-11-30 11:59:12 +0100

    click on canvas catches mouse pointer

--- src/game.js
+++ src/game.js
@@ -1,6 +1,22 @@
 Game = {
 	init: function () {
-		Crafty.init(1000, 600);
+		Crafty.init(1000, 600, document.getElementById('canvas'));
 		Crafty.background('gray');
+		Game.start();
+	},
+	lockPointer: function(element) {
+		var havePointerLock = 'pointerLockElement' in document ||
+			'mozPointerLockElement' in document ||
+			'webkitPointerLockElement' in document;
+		element.requestPointerLock = element.requestPointerLock ||
+			element.mozRequestPointerLock ||
+			element.webkitRequestPointerLock;
+		return element.requestPointerLock();
+	},
+	start: function() {
+		var player = Crafty.e('2D, Canvas, Fourway, Color').attr({'x': 50, 'y': 50, 'h': 60, 'w': 30}).fourway(5).color('#dddddd');
+		var turret = Crafty.e('2D, Canvas, Color').attr({'x': 80, 'y': 100, 'h': 10, 'w': 10}).color('#ff0000');
+
+		player.attach(turret);
 	}
 };
