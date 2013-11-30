ef659e79c2d8d2ac6d6f27e455f5b6164934d242
Kana <kanabanarama@googlemail.com>
2013-11-30 18:15:24 +0100

    parallax working

--- src/game.js
+++ src/game.js
@@ -27,31 +27,37 @@ Game = {
 				.text(loadingText)
 				.attr({ x: Game.width/2 - loadingText.length*5/2, y: Game.height/2 - 24, w: Game.width });
 
-			Crafty.load(['assets/step01_basket.png', 'assets/step01_balloon.png', 'assets/parallax.png'], function() {
+			Crafty.load([
+					'assets/step01_basket.png',
+					'assets/step01_balloon.png',
+					'assets/parallax00.png',
+					'assets/parallax01.png',
+					'assets/parallax02.png'
+				], function() {
 				Crafty.sprite(50, 84, 'assets/step01_basket.png', {
 					blimp_base_01: [0, 0]
 				});
 				Crafty.sprite(50, 84, 'assets/step01_balloon.png', {
 					blimp_drive_01: [0, 0]
 				});
-				Crafty.sprite(2000, 600, "assets/parallax.png", {
+				Crafty.sprite(2000, 600, "assets/parallax00.png", {
+					background_layer_00: [0,0]
+				});
+				Crafty.sprite(2000, 600, "assets/parallax01.png", {
 					background_layer_01: [0,0]
 				});
-				/*Crafty.sprite(2000, 600, "assets/parallax.png", {
+				Crafty.sprite(2000, 600, "assets/parallax02.png", {
 					background_layer_02: [0,0]
 				});
-				Crafty.sprite(2000, 600, "assets/parallax.png", {
-					background_layer_03: [0,0]
-				});*/
 
 				Crafty.scene('Main');
 			})
 		});
 
 		Crafty.scene('Main', function() {
-			Crafty.e("2D, Canvas, Image")
-				.attr({ x: 0, h: 0, w: Crafty.viewport.width*5, h: Crafty.viewport.height })
-				.image("assets/parallax.png", "repeat");
+			Crafty.e('Parallax').background('assets/parallax00.png').parallax(0);
+			Crafty.e('Parallax').background('assets/parallax01.png').parallax(1);
+			Crafty.e('Parallax').background('assets/parallax02.png').parallax(1.5);
 
 			Crafty.e('Player').attr({ x: 50, y: 50 });
 
