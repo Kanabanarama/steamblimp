de60ebf3ed3f666be33ebb51f75e080e3dd04c62
Kana <kanabanarama@googlemail.com>
2013-11-30 14:56:07 +0100

    scrolling

--- src/components/player.js
+++ src/components/player.js
@@ -1,5 +1,8 @@
 Crafty.c('Player', {
 	init: function() {
-		this.requires('2D, Canvas, Color, Controls');
+		this.requires('2D, Canvas, Controls');
+		this.bind('EnterFrame', function() {
+			this.attr({ 'x': this._x+1 });
+		})
 	}
 });
--- src/game.js
+++ src/game.js
@@ -1,6 +1,10 @@
 Game = {
 	width: 1000,
 	height: 600,
+	scrollspeed: {
+		x: 1,
+		y: 0
+	},
 	init: function () {
 		Crafty.init(Game.width, Game.height, document.getElementById('canvas'));
 		Crafty.background('gray');
@@ -23,22 +27,32 @@ Game = {
 				.text(loadingText)
 				.attr({ x: Game.width/2 - loadingText.length*5/2, y: Game.height/2 - 24, w: Game.width });
 
-			Crafty.load(['assets/blimp.png'], function() {
-				Crafty.sprite(60, 'assets/blimp.png', {
-					blimp_rank_01: [0, 0],
-					blimp_rank_02: [1, 0],
-					blimp_rank_03: [0, 1],
-					blimp_rank_04: [1, 1]
+			Crafty.load(['assets/step01_basket.png', 'assets/step01_balloon.png', 'assets/parallax.png'], function() {
+				Crafty.sprite(50, 84, 'assets/step01_basket.png', {
+					blimp_base_01: [0, 0]
+				});
+				Crafty.sprite(50, 84, 'assets/step01_balloon.png', {
+					blimp_drive_01: [0, 0]
+				});
+				Crafty.sprite(2400, 1200, "assets/parallax.png", {
+					background: [0,0]
 				});
 
-				//Crafty.scene('Main');
+				Crafty.scene('Main');
 			})
 		});
 
 		Crafty.scene('Main', function() {
-			var player = Crafty.e('Player').attr( {'x': 50, 'y': 50, 'h': 60, 'w': 30} ).fourway(5).color('#dddddd');
-			var turret = Crafty.e('2D, Canvas, Color').attr( {'x': 80, 'y': 100, 'h': 10, 'w': 10} ).color('#ff0000');
+			var background = Crafty.e('2D, Canvas, background').attr( {'x': 0, 'y': -300, 'w': 2400, 'h': 1200} );
+			var player = Crafty.e('Player, blimp_base_01').attr( {'x': 50, 'y': 50, 'w': 50, 'h': 84} ).fourway(5);
+			var turret = Crafty.e('2D, Canvas, blimp_drive_01').attr( {'x': 50, 'y': 50, 'w': 50, 'h': 84} );
 			player.attach(turret);
+
+			Crafty.bind('EnterFrame', Game.gameLoop);
 		});
+	},
+	gameLoop: function() {
+		//console.log(Crafty.frameTime);
+		Crafty.viewport.x -= Game.scrollspeed.x;
 	}
 };
