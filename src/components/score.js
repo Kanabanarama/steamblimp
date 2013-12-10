81527022b3dcc46622d9f08992e9715368d86a1d
kay <k@kay.is>
2013-12-10 21:52:46 +0100

    removed score log

--- src/components/score.js
+++ src/components/score.js
@@ -3,7 +3,6 @@ Crafty.c('Score', {
 	score: 0,
 
 	init: function () {
-		console.debug('SCORE');
 		this.requires('2D, Persist, Canvas, Text')
 			.attr({x: 15, y: 15})
 			.textFont({size: '20px', family: 'Georgia, serif'})
